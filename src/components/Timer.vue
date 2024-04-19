838913a152c0216e4e6cf1f0371ea6ca7d693839
littlesekii <littlesekii@gmail.com>
2024-04-19 11:24:51 -0300

    implemented countdown logic

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,19 +1,57 @@
 <script setup>
+import { reactive } from 'vue';
+
+function zeroFill(value) {
+    return ('00'+value).slice(-2);
+}
+
+const timerValues = reactive({minutes: 1, seconds: 10});
+
+let timer;
+function startTimer() {
+    console.log("timer started");
+    timer = setInterval(executeTimer, 1000);
+}
+
+function stopTimer() {
+    console.log("timer finished");
+    clearInterval(timer);
+}
+
+function executeTimer() {
+    console.log("timer tick");
+    timerValues.seconds--;
+
+    if (timerValues.seconds === 0) {
+        if (timerValues.minutes === 0) {
+            stopTimer();
+            return;
+        }
+        
+        timerValues.seconds = 59;
+        timerValues.minutes--;            
+    }
+
+    
+}
 
 </script>
 
 <template>
     <div class="timer">
         <div class="timer-display">
-            <div class="timer-display-number dn-minutes">30</div>
+            <div class="timer-display-number dn-minutes">{{ zeroFill(timerValues.minutes) }}</div>
             <div class="timer-display-number dn-sep">:</div>
-            <div class="timer-display-number dn-seconds">00</div>
+            <div class="timer-display-number dn-seconds">{{ zeroFill(timerValues.seconds) }}</div>
         </div>
 
         <div class="timer-controls">
-            <button class="timer-controls-button tcb-toggle">
+            <button class="timer-controls-button tcb-toggle" @click="startTimer()">
                 START
             </button>
+            <!-- <button class="timer-controls-button tcb-toggle" @click="stopTimer()">
+                STOP
+            </button> -->
             <!-- <button class="timer-controls-button tcb-toggle">
                 RESET
             </button> -->
